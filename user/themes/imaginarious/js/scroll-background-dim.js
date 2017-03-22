f9760f0fc982af508e0c4739fa77374c328fd41d
Philo van Kemenade <philovankemenade@gmail.com>
2017-03-22 11:19:34 +0100

    fix background dimming on scroll

--- user/themes/imaginarious/js/scroll-background-dim.js
+++ user/themes/imaginarious/js/scroll-background-dim.js
@@ -1,26 +1,26 @@
 var isTouch = window.DocumentTouch && document instanceof DocumentTouch;
 
-function dimBackground() {
-    var zvalue = $(document).scrollTop();
+function dimBackground($container) {
+    var zvalue = $container.scrollTop();
     if ( zvalue > 800 )
-        $("#sb-site").addClass("scrolled");
+        $container.addClass("scrolled");
     else
-        $("#sb-site").removeClass("scrolled");
+        $container.removeClass("scrolled");
 }
 
 jQuery(document).ready(function($){
-
+    var $container = $('#sb-site');
     // ON SCROLL EVENTS
     if (!isTouch){
-        $(document).scroll(function() {
-            dimBackground();
+        $container.scroll(function() {
+            dimBackground($container);
         });
     };
 
     // TOUCH SCROLL
-    $(document).on({
+    $container.on({
         'touchmove': function(e) {
-            dimBackground();
+            dimBackground($container);
         }
     });
 });
