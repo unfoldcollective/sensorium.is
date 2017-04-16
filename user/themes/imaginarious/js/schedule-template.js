9017f0e5462919b6bf275d87da7b63d549479d68
Philo van Kemenade <philovankemenade@gmail.com>
2017-04-16 15:26:00 +0200

    only load content when source name is specified in data-content attribute

--- user/themes/imaginarious/js/schedule-template.js
+++ user/themes/imaginarious/js/schedule-template.js
@@ -119,10 +119,13 @@ jQuery(document).ready(function($){
 		this.modal.attr('data-event', event.parent().attr('data-event'));
 
 		//update event content
-		this.modalBody.find('.event-info').load(event.parent().attr('data-content')+'.html .event-info > *', function(data){
-			//once the event content has been loaded
-			self.element.addClass('content-loaded');
-		});
+		var contentSourceName = event.parent().attr('data-content');
+		if (contentSourceName) {
+			this.modalBody.find('.event-info').load(contentSourceName+'.html .event-info > *', function(data){
+				//once the event content has been loaded
+				self.element.addClass('content-loaded');
+			});
+		}
 
 		this.element.addClass('modal-is-open');
 
